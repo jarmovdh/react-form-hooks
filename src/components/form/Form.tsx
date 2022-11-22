932a58b1452f06e44678a9d6ed7350181bd3c0d8
Your Name <jarmovanderheul@MBP-van-Jarmo.localdomain>
2022-11-22 11:39:28 +0100

    confirm password

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as Styled from "./Form.styled";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import * as Yup from "yup";
 
 type formInput = {
   firstName: string;
@@ -13,21 +13,18 @@ type formInput = {
   comfirmPassword: string;
 };
 
-const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required("Last name is required!"),
-  email: yup.string().email().required("Email is required!"),
-  age: yup
-    .number()
-    .typeError("age must be a number")
-    .positive("age must be greater than zero")
-    .required("age is required"),
-  password: yup
-    .string()
-    .min(4, "Password length should be > 4 characters")
-    .max(15)
-    .required(),
-  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+const schema = Yup.object().shape({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required("Last name is required!"),
+  email: Yup.string().email().required("Email is required!"),
+  age: Yup.number()
+    .typeError("Age must be a number")
+    .positive("Age must be greater than zero")
+    .required("Age is required"),
+  password: Yup.string().min(4, "Password length should be > 4 characters").max(15).required(),
+  comfirmPassword: Yup.string()
+    .required("Password is required")
+    .oneOf([Yup.ref("password")], "Passwords must match"),
 });
 
 export const Form = () => {
@@ -41,7 +38,7 @@ export const Form = () => {
 
   const submitForm = handleSubmit((data) => {
     console.log(data);
-	});
+  });
 
   return (
     <>
@@ -49,42 +46,22 @@ export const Form = () => {
         <Styled.Title>Sign Up</Styled.Title>
         <div className="inputs">
           <Styled.Form onSubmit={submitForm}>
-            <Styled.Input
-              type="text"
-              placeholder="First Name..."
-              {...register("firstName")}
-            />
+            <Styled.Input type="text" placeholder="First Name..." {...register("firstName")} />
             <p>{errors.firstName?.message}</p>
-            <Styled.Input
-              type="text"
-              placeholder="Last Name..."
-              {...register("lastName")}
-            />
+            <Styled.Input type="text" placeholder="Last Name..." {...register("lastName")} />
             <p>{errors.lastName?.message}</p>
-            <Styled.Input
-              type="text"
-              placeholder="Email..."
-              {...register("email")}
-            />
+            <Styled.Input type="text" placeholder="Email..." {...register("email")} />
             <p>{errors.email?.message}</p>
-            <Styled.Input
-              type="text"
-              placeholder="Age..."
-              {...register("age")}
-            />
+            <Styled.Input type="text" placeholder="Age..." {...register("age")} />
             <p>{errors.age?.message}</p>
-            <Styled.Input
-              type="password"
-              placeholder="Password..."
-              {...register("password")}
-            />
+            <Styled.Input type="password" placeholder="Password..." {...register("password")} />
             <p>{errors.password?.message}</p>
             <Styled.Input
               type="password"
               placeholder="Confirm Password..."
-              {...register("password")}
+              {...register("comfirmPassword")}
             />
-            <p>{errors.password && "Password should match!"}</p>
+            <p>{errors.comfirmPassword?.message && "Password should match!"}</p>
             <Styled.Submit type="submit" id="sumbit" value="Submit" />
           </Styled.Form>
         </div>
