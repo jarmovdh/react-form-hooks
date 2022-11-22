ba9c5180d135670343acf8dfb223b069f935d269
Your Name <jarmovanderheul@MBP-van-Jarmo.localdomain>
2022-11-22 10:41:36 +0100

    custom error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { Form } from './components/form';
+import React from "react";
+import "./App.css";
+import { Form } from "./components/form";
 
 function App() {
   return (
--- src/components/form/Form.styled.ts
+++ src/components/form/Form.styled.ts
@@ -1,12 +1,33 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  border: 1px solid #497174;
+  border-radius: 5px;
+  background-color: #eff5f5;
+`;
+
 export const Form = styled.form`
-display: flex;
-flex-direction: column;
-padding: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
 `;
 
 export const Input = styled.input`
-margin: 10px;
-padding: 10px;
-`
+  margin: 10px;
+  padding: 10px;
+`;
+
+export const Submit = styled.input`
+  background-color: #5f8d4e;
+  border: 1px solid #497174;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 10px;
+  padding: 10px;
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  margin: 10px;
+  padding: 10px;
+`;
